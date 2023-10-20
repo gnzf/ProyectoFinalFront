e6b2942ce0d9af37148d97f76a3899d868ce7653
¨JuanPedroNodar¨ <juanpedro.nodar@gmail.com>
2023-10-20 19:28:14 -0300

    commit juanpe

---
+++ src/components/registro/first page/InputGeneral.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../../styles/registro/first page/InputGeneral.css";
+
+function InputGeneral() {
+  return (
+    <>
+      <input className="input-general" type="email"></input>
+    </>
+  );
+}
+
+export default InputGeneral;
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import InicioDeSesion from './pages/inicioDeSesion'
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Registro from "./pages/registro";
+import Home from "./pages/home";
+import RegistoCrearCuenta from "./pages/RegistoCrearCuenta";
 
 const router = createBrowserRouter([
-  {
+  /*   {
     path: "/home",
-    element: <Home/>
+    element: <Home />,
+  }, */
+  /*   {
+    path: "/login",
+    element: <InicioDeSesion />,
+  },
+ */
+  {
+    path: "/register",
+    element: <Registro />,
   },
   {
-    path: "/login",
-    element: <InicioDeSesion/>
+    path: "/create-account" /*En realidad creo que deberia ser path: "/register/create-account" */,
+    element: <RegistoCrearCuenta />,
   },
+]);
 
-  
-])
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider  router={router} />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
