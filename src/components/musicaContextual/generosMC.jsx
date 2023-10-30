7d16162baf2afb1c3d4546982dd9904b24e2d18d
geral <aalvarezgeraldine@gmail.com>
2023-10-30 20:28:24 -0300

    generoMC

--- src/components/musicaContextual/generosMC.jsx
+++ src/components/musicaContextual/generosMC.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import "./generosMC.css"
+import React, { useState, useEffect } from "react";
+import "./generosMC.css";
 
 function GenerosMC(props) {
+  const [currentClass, setCurrentClass] = useState("cuadradoBlanco");
 
-    
-
+  function cambiarColor() {
+    if (currentClass === "cuadradoBlanco") {
+      setCurrentClass("cuadradoNegro");
+    } else {
+      setCurrentClass("cuadradoBlanco");
+    }
+  }
   return (
     <div>
-        <div className='container-generosmc'>
-            <button>{props.generoName}</button>
-        </div>
+      <div className="container-generosmc">
+        <button className={currentClass} onClick={cambiarColor}>
+          {props.generoName}
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default GenerosMC;
