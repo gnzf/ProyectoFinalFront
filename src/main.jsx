a3d643b2aa18afa6974ada4131778060fe4f17c3
gonzalo fernandez <gonchjj.14@gmail.com>
2023-11-08 21:41:24 -0300

    probando vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import InicioDeSesion from './pages/InicioDeSesion'
+import InicioDeSesion from './pages/inicioDeSesion'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import InicioDeApp from "./pages/iniciodeapp";
 import Carrusel from "./components/cupidomusical/Carrusel";
