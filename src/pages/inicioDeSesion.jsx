b266c521bcaa5be5e6225a02060396b9c266231c
gonzalo fernandez <gonchjj.14@gmail.com>
2023-11-08 22:08:25 -0300

    probando vercel

--- src/pages/inicioDeSesion.jsx
+++ src/pages/inicioDeSesion.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/inicioDeSesion/inicioDeSesion.css";
-import TitleArrow from "../components/InicioDeSesion/TitleArrow";
+import TitleArrow from "../components/iniciodesesion/TitleArrow";
 import {
   FieldsetEmailUser,
   FieldSetPassword,
